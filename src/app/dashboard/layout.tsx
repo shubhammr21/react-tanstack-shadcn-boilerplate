bedcb52faa1f8e18f54799288f242462a01c2fb8
Shubham Maurya <shubhammr21@gmail.com>
2025-01-12 02:05:11 +0530

    feat(dashboard): enhance header UI and layout styles
    - Add sticky header with rounded background
    - Add page title in header
    - Adjust spacing and layout for header and main content
    - Remove global transition effect for color changes
    
    This commit updates the UI and layout styles for the dashboard page.
    The header section now has a sticky and rounded background, along with a page title.
    The spacing and layout of the header and main content area have been adjusted for better visual alignment and spacing.
    Additionally, the global transition effect for color changes has been removed to improve performance.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -22,8 +22,9 @@ export default function DashboardLayout() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b pl-2 pr-4">
+        <header className="sticky top-0 z-10 flex h-16 items-center gap-2 rounded-md bg-inherit pl-2 pr-4">
           <SidebarTrigger className="-ml-1" />
+          <h1 className="text-xl font-semibold">Playground</h1>
           <Separator orientation="vertical" className="mr-2 h-4" />
           <Breadcrumb>
             <BreadcrumbList>
@@ -38,10 +39,10 @@ export default function DashboardLayout() {
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
-          <div className="flex-grow"></div>
+          <div className="flex-grow" />
           <ModeToggle />
         </header>
-        <main className="flex flex-1 flex-col gap-4 py-4 pl-2 pr-4">
+        <main className="flex flex-1 flex-col gap-4 pb-2 pl-2 pr-4 pt-4">
           <Outlet />
         </main>
       </SidebarInset>
